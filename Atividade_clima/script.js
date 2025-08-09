acb84dcbb7fd2538df17d4e11596ad98064a6125
Ary <mags12@aluno.ifal.edu.br>
2025-08-09 02:08:06 -0300

    Add files via upload

---
+++ Atividade_clima/script.js
@@ -0,0 +1,61 @@
+//VARIÁVEIS E SELEÇÃO DE ELEMENTOS
+
+const apiKey = "b3f8141209c14678889213747250808";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const countryElement = document.querySelector("#country")
+const localTimeElement = document.querySelector("#localTime");
+const tempElement = document.querySelector("#temperature");
+const condiElement = document.querySelector("#condition");
+const sensElement = document.querySelector("#feelsLike");
+const weatherIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#humidity");
+const windElement = document.querySelector("#wind");
+const pressureElement = document.querySelector("#pressure");
+const visibilityElement = document.querySelector("#visibility");
+const indElement = document.querySelector("#uv-index");
+
+const weatherContainer = document.querySelector("#weather-result");
+
+//FUNÇÕES
+
+const getWeatherData = async(city) => {
+
+    const apiweatherURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no&lang=pt`;
+
+    const res = await fetch(apiweatherURL);
+    const data = await res.json();
+
+    return data;
+    //console.log(data); => testar e ver o nome das variáveis para exibi-lás
+};
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = `${data.location.name}, ${data.location.country}`;
+    localTimeElement.innerText = data.location.localtime;
+    tempElement.innerText = `${parseInt(data.current.temp_c)}°C`;
+    condiElement.innerText = data.current.condition.text;
+    weatherIconElement.setAttribute("src", `https:${data.current.condition.icon}`);
+    umidityElement.innerText = `${data.current.humidity}%`;
+    windElement.innerText = `${data.current.wind_kph}km/h`;
+    pressureElement.innerText = `${data.current.pressure_mb} mb`;
+    sensElement.innerText = data.current.feelslike_c;
+    visibilityElement.innerText = `${data.current.vis_km} km`;
+    indElement.innerText = data.current.uv;
+
+    weatherContainer.classList.remove("hidden");
+};
+
+//EVENTOS
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
